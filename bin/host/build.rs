592363c919d614a47e7e48fa1aab8bb9c32322b6
Jonathan LEI <me@xjonathan.dev>
2024-08-28 04:49:06 +0800

    chore: use a newer `reth` patch from `sp1-patches`

--- bin/host/build.rs
+++ bin/host/build.rs
@@ -1,6 +1,12 @@
 use sp1_helper::{build_program_with_args, BuildArgs};
 
 fn main() {
-    build_program_with_args(&format!("../{}", "client-eth"), BuildArgs { ..Default::default() });
-    build_program_with_args(&format!("../{}", "client-op"), BuildArgs { ..Default::default() });
+    build_program_with_args(
+        &format!("../{}", "client-eth"),
+        BuildArgs { ignore_rust_version: true, ..Default::default() },
+    );
+    build_program_with_args(
+        &format!("../{}", "client-op"),
+        BuildArgs { ignore_rust_version: true, ..Default::default() },
+    );
 }
--- crates/primitives/src/account_proof.rs
+++ crates/primitives/src/account_proof.rs
@@ -20,7 +20,9 @@ impl AccountProofWithBytecode {
 
     /// Verifies the account proof against the provided state root.
     pub fn verify(&self, state_root: B256) -> eyre::Result<()> {
-        self.proof.verify(state_root)?;
+        self.proof
+            .verify(state_root)
+            .map_err(|err| eyre::eyre!("Account proof verification failed: {err}"))?;
         if let Some(info) = &self.proof.info {
             if info.bytecode_hash.unwrap() != keccak256(self.code.bytes()) {
                 return Err(eyre::eyre!("Code hash does not match the code"));
@@ -35,7 +37,6 @@ pub fn eip1186_proof_to_account_proof(proof: EIP1186AccountProofResponse) -> Acc
     let address = proof.address;
     let balance = proof.balance;
     let code_hash = proof.code_hash;
-    let nonce = proof.nonce.as_limbs()[0];
     let storage_root = proof.storage_hash;
     let account_proof = proof.account_proof;
     let storage_proofs = proof
@@ -46,18 +47,21 @@ pub fn eip1186_proof_to_account_proof(proof: EIP1186AccountProofResponse) -> Acc
             let value = storage_proof.value;
             let proof = storage_proof.proof;
             let mut sp = StorageProof::new(key.0);
-            sp.set_value(value);
-            sp.set_proof(proof);
+            sp.value = value;
+            sp.proof = proof;
             sp
         })
         .collect();
 
     let (storage_root, info) =
-        if nonce == 0 && balance.is_zero() && storage_root.is_zero() && code_hash.is_zero() {
+        if proof.nonce == 0 && balance.is_zero() && storage_root.is_zero() && code_hash.is_zero() {
             // Account does not exist in state. Return `None` here to prevent proof verification.
             (EMPTY_ROOT_HASH, None)
         } else {
-            (storage_root, Some(Account { nonce, balance, bytecode_hash: code_hash.into() }))
+            (
+                storage_root,
+                Some(Account { nonce: proof.nonce, balance, bytecode_hash: code_hash.into() }),
+            )
         };
 
     AccountProof { address, info, proof: account_proof, storage_root, storage_proofs }
