6f97f0c2a2ea50ff47dc9110602b4a4fd2913d60
Yuwen Zhang <yuwen01@gmail.com>
2024-11-08 19:38:05 -0800

    build program standard

--- bin/host/build.rs
+++ bin/host/build.rs
@@ -1,16 +1,7 @@
-use sp1_helper::{build_program_with_args, BuildArgs};
+use sp1_helper::build_program;
 
 fn main() {
-    build_program_with_args(
-        &format!("../{}", "client-eth"),
-        BuildArgs { ignore_rust_version: true, ..Default::default() },
-    );
-    build_program_with_args(
-        &format!("../{}", "client-op"),
-        BuildArgs { ignore_rust_version: true, ..Default::default() },
-    );
-    build_program_with_args(
-        &format!("../{}", "client-linea"),
-        BuildArgs { ignore_rust_version: true, ..Default::default() },
-    );
+    build_program("../client-eth");
+    build_program("../client-op");
+    build_program("../client-linea");
 }
