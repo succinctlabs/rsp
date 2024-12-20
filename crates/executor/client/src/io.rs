ee3762017dd5b1a2158b1b31b7dd01520092874b
nhtyy <n@push0.dev>
2024-12-19 16:40:45 -0800

    feat: initial trie db

--- crates/executor/client/src/io.rs
+++ crates/executor/client/src/io.rs
@@ -2,12 +2,15 @@ use std::{collections::HashMap, iter::once};
 
 use eyre::Result;
 use itertools::Itertools;
+use reth_errors::ProviderError;
 use reth_primitives::{revm_primitives::AccountInfo, Address, Block, Header, B256, U256};
 use reth_trie::TrieAccount;
 use revm_primitives::{keccak256, Bytecode};
 use rsp_mpt::EthereumState;
-use rsp_witness_db::WitnessDb;
+//use rsp_witness_db::WitnessDb;
 use serde::{Deserialize, Serialize};
+
+use revm::DatabaseRef;
 
 /// The input for the client to execute a block and fully verify the STF (state transition
 /// function).
@@ -37,7 +40,7 @@ impl ClientExecutorInput {
     }
 
     /// Creates a [`WitnessDb`].
-    pub fn witness_db(&self) -> Result<WitnessDb> {
+    pub fn witness_db(&self) -> Result<TrieDB<'_>> {
         <Self as WitnessInput>::witness_db(self)
     }
 }
@@ -66,6 +69,78 @@ impl WitnessInput for ClientExecutorInput {
     #[inline(always)]
     fn headers(&self) -> impl Iterator<Item = &Header> {
         once(&self.current_block.header).chain(self.ancestor_headers.iter())
+    }
+}
+
+#[derive(Debug)]
+pub struct TrieDB<'a> {
+    inner: &'a EthereumState,
+    block_hashes: HashMap<u64, B256>,
+    bytecode_by_hash: HashMap<B256, &'a Bytecode>,
+}
+
+impl<'a> TrieDB<'a> {
+    pub fn new(
+        inner: &'a EthereumState,
+        block_hashes: HashMap<u64, B256>,
+        bytecode_by_hash: HashMap<B256, &'a Bytecode>,
+    ) -> Self {
+        Self { inner, block_hashes, bytecode_by_hash }
+    }
+}
+
+impl<'a> DatabaseRef for TrieDB<'a> {
+    /// The database error type.
+    type Error = ProviderError;
+
+    /// Get basic account information.
+    fn basic_ref(&self, address: Address) -> Result<Option<AccountInfo>, Self::Error> {
+        let hashed_address = keccak256(address);
+        let hashed_address = hashed_address.as_slice();
+
+        let account_in_trie = self.inner.state_trie.get_rlp::<TrieAccount>(hashed_address).unwrap();
+
+        let account = account_in_trie.map(|account_in_trie| AccountInfo {
+            balance: account_in_trie.balance,
+            nonce: account_in_trie.nonce,
+            code_hash: account_in_trie.code_hash,
+            code: None,
+        });
+
+        Ok(account)
+    }
+
+    /// Get account code by its hash.
+    fn code_by_hash_ref(&self, hash: B256) -> Result<Bytecode, Self::Error> {
+        Ok(self.bytecode_by_hash
+            .get(&hash)
+            .map(|code| (*code).clone())
+            .unwrap())
+    }
+
+    /// Get storage value of address at index.
+    fn storage_ref(&self, address: Address, index: U256) -> Result<U256, Self::Error> {
+        let hashed_address = keccak256(address);
+        let hashed_address = hashed_address.as_slice();
+
+        let storage_trie = self
+            .inner
+            .storage_tries
+            .get(hashed_address)
+            .expect("A storage trie must be provided for each account");
+
+        Ok(storage_trie
+            .get_rlp::<U256>(keccak256(index.to_be_bytes::<32>()).as_slice())
+            .expect("Can get from MPT")
+            .unwrap_or_default())
+    }
+
+    /// Get block hash by block number.
+    fn block_hash_ref(&self, number: u64) -> Result<B256, Self::Error> {
+        Ok(*self
+            .block_hashes
+            .get(&number)
+            .expect("A block hash must be provided for each block number"))
     }
 }
 
@@ -98,7 +173,7 @@ pub trait WitnessInput {
     /// implementing this trait causes a zkVM run to cost over 5M cycles more. To avoid this, define
     /// a method inside the type that calls this trait method instead.
     #[inline(always)]
-    fn witness_db(&self) -> Result<WitnessDb> {
+    fn witness_db(&self) -> Result<TrieDB<'_>> {
         let state = self.state();
 
         if self.state_anchor() != state.state_root() {
@@ -107,52 +182,6 @@ pub trait WitnessInput {
 
         let bytecodes_by_hash =
             self.bytecodes().map(|code| (code.hash_slow(), code)).collect::<HashMap<_, _>>();
-
-        let mut accounts = HashMap::new();
-        let mut storage = HashMap::new();
-        for (&address, slots) in self.state_requests() {
-            let hashed_address = keccak256(address);
-            let hashed_address = hashed_address.as_slice();
-
-            let account_in_trie = state.state_trie.get_rlp::<TrieAccount>(hashed_address)?;
-
-            accounts.insert(
-                address,
-                match account_in_trie {
-                    Some(account_in_trie) => AccountInfo {
-                        balance: account_in_trie.balance,
-                        nonce: account_in_trie.nonce,
-                        code_hash: account_in_trie.code_hash,
-                        code: Some(
-                            (*bytecodes_by_hash
-                                .get(&account_in_trie.code_hash)
-                                .ok_or_else(|| eyre::eyre!("missing bytecode"))?)
-                            // Cloning here is fine as `Bytes` is cheap to clone.
-                            .to_owned(),
-                        ),
-                    },
-                    None => Default::default(),
-                },
-            );
-
-            if !slots.is_empty() {
-                let mut address_storage = HashMap::new();
-
-                let storage_trie = state
-                    .storage_tries
-                    .get(hashed_address)
-                    .ok_or_else(|| eyre::eyre!("parent state does not contain storage trie"))?;
-
-                for &slot in slots {
-                    let slot_value = storage_trie
-                        .get_rlp::<U256>(keccak256(slot.to_be_bytes::<32>()).as_slice())?
-                        .unwrap_or_default();
-                    address_storage.insert(slot, slot_value);
-                }
-
-                storage.insert(address, address_storage);
-            }
-        }
 
         // Verify and build block hashes
         let mut block_hashes: HashMap<u64, B256> = HashMap::new();
@@ -168,6 +197,6 @@ pub trait WitnessInput {
             block_hashes.insert(parent_header.number, child_header.parent_hash);
         }
 
-        Ok(WitnessDb { accounts, storage, block_hashes })
+        Ok(TrieDB::new(state, block_hashes, bytecodes_by_hash))
     }
 }
