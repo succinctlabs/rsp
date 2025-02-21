15e31725732696a3d4fafef9f335d85658738eaf
Kautuk Kundan <kautuk@stackrlabs.xyz>
2025-02-21 14:48:47 -0800

    added retry for failed blocks

--- bin/host/src/continuous.rs
+++ bin/host/src/continuous.rs
@@ -3,12 +3,13 @@ use crate::{
     db,
     execute::execute,
 };
+
 use alloy_provider::{Provider, ProviderBuilder};
 use futures_util::{future::join_all, StreamExt};
 use op_alloy_network::Ethereum;
 use rsp_host_executor::create_eth_block_execution_strategy_factory;
 use rsp_primitives::genesis::Genesis;
-use std::time::SystemTime;
+use std::{pin::Pin, time::SystemTime};
 use tokio::task;
 
 pub async fn run_continuous_mode(
@@ -27,10 +28,7 @@ pub async fn run_continuous_mode(
     let ws = alloy::providers::WsConnect::new(ws_url);
     let provider = ProviderBuilder::new().on_ws(ws).await?;
     let subscription = provider.subscribe_blocks().await?;
-    let mut stream = subscription.into_stream();
-
-    let block_execution_strategy_factory =
-        create_eth_block_execution_strategy_factory(&genesis, args.custom_beneficiary);
+    let mut stream = subscription.into_stream().take(3);
 
     let mut handles: Vec<task::JoinHandle<()>> = Vec::new();
 
@@ -38,44 +36,21 @@ pub async fn run_continuous_mode(
         let block_num = block.number;
         println!("Received block: {:?}", block_num);
 
-        let args_clone = args.clone();
+        // Clone the args and modify the block number with the one received from the stream
+        let mut modified_args = args.clone();
+        modified_args.block_number = Some(block_num);
+
         let provider_config_clone = provider_config.clone();
         let genesis_clone = genesis.clone();
-        let block_execution_strategy_factory_clone = block_execution_strategy_factory.clone();
 
-        // Spawn a new task for this block
+        // Spawn a new task to process the block
         let handle = task::spawn(async move {
-            let mut new_args = args_clone;
-            let another_clone = new_args.clone();
-            new_args.block_number = Some(block_num);
-
-            println!("Processing block {}", block_num);
-
-            let result = execute::<Ethereum, _, _>(
-                new_args,
-                provider_config_clone,
-                genesis_clone,
-                None,
-                block_execution_strategy_factory_clone,
-                false,
-            )
-            .await;
-
-            match result {
-                Ok(_) => println!("Successfully processed block {}", block_num),
-                Err(e) => {
-                    // update block status as failed
-                    let pool = db::init_db_pool(&another_clone.db_url).await.unwrap();
-                    let end_time = db::system_time_to_timestamp(SystemTime::now());
-                    db::update_block_status_as_failed(&pool, block_num as i64, end_time)
-                        .await
-                        .unwrap();
-
-                    eprintln!("Error processing block {}: {}", block_num, e)
-                }
+            if let Err(e) =
+                spawn_block_processing_task(modified_args, provider_config_clone, genesis_clone)
+                    .await
+            {
+                eprintln!("Block processing failed: {}", e);
             }
-
-            println!("Processed block {}", block_num);
         });
 
         handles.push(handle);
@@ -97,5 +72,58 @@ pub async fn run_continuous_mode(
         }
     }
 
+    Ok(())
+}
+
+pub async fn spawn_block_processing_task(
+    args: HostArgs,
+    provider_config: ProviderConfig,
+    genesis: Genesis,
+) -> eyre::Result<()> {
+    let block_num = args.block_number.unwrap();
+    println!("Processing block {:?}", &block_num);
+
+    let block_execution_strategy_factory =
+        create_eth_block_execution_strategy_factory(&genesis, args.custom_beneficiary);
+
+    match execute::<Ethereum, _, _>(
+        args.clone(),
+        provider_config.clone(),
+        genesis.clone(),
+        None,
+        block_execution_strategy_factory,
+        false,
+    )
+    .await
+    {
+        Ok(_) => {
+            println!("Successfully processed block {:?}", &block_num);
+            Ok(())
+        }
+        Err(e) => {
+            if let Err(db_err) = update_block_status_as_failed(&args.db_url, block_num).await {
+                eprintln!("Database error updating block {:?}: {}", &block_num, db_err);
+            }
+            eprintln!("Error processing block {:?}: {}", &block_num, e);
+
+            // Recursive retry
+            //
+            // [IMPORTANT] this recursive retry is not ideal, but it's a quick fix to handle failures
+            // it is assumed that the error is transient and will be resolved in the next attempt
+            println!("Retrying block {:?}...", &block_num);
+            Pin::from(Box::new(spawn_block_processing_task(
+                args.clone(),
+                provider_config.clone(),
+                genesis.clone(),
+            )))
+            .await
+        }
+    }
+}
+
+async fn update_block_status_as_failed(db_url: &str, block_num: u64) -> eyre::Result<()> {
+    let pool = db::init_db_pool(db_url).await?;
+    let end_time = db::system_time_to_timestamp(SystemTime::now());
+    db::update_block_status_as_failed(&pool, block_num as i64, end_time).await?;
     Ok(())
 }
