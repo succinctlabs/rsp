89c2b9cbf3306659d01ff15b4e24b774bc977581
Aurélien <3535019+leruaa@users.noreply.github.com>
2025-03-08 10:13:38 +0100

    feat: dockerize continuous and eth-proofs exe (#95)
    * feat: Dockerize continuous and eth-proofs exe
    
    * fix: nit

--- bin/eth-proofs/src/main.rs
+++ bin/eth-proofs/src/main.rs
@@ -27,7 +27,15 @@ async fn main() -> eyre::Result<()> {
     }
 
     // Initialize the logger.
-    tracing_subscriber::registry().with(fmt::layer()).with(EnvFilter::from_default_env()).init();
+    tracing_subscriber::registry()
+        .with(fmt::layer())
+        .with(
+            EnvFilter::from_default_env()
+                .add_directive("sp1_core_machine=warn".parse().unwrap())
+                .add_directive("sp1_core_executor=warn".parse().unwrap())
+                .add_directive("sp1_prover=warn".parse().unwrap()),
+        )
+        .init();
 
     // Parse the command line arguments.
     let args = Args::parse();
