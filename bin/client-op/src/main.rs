2496ae1d26e90edd4aad230ddf09bf8854b4e03b
Aurélien <3535019+leruaa@users.noreply.github.com>
2025-05-14 20:49:30 +0200

    feat: use profile_report! in the client (#130)
    * feat: use profile_report! in the client
    
    * fix: CI

--- bin/client-op/src/main.rs
+++ bin/client-op/src/main.rs
@@ -4,15 +4,16 @@ sp1_zkvm::entrypoint!(main);
 use rsp_client_executor::{
     executor::{OpClientExecutor, DESERIALZE_INPUTS},
     io::{CommittedHeader, OpClientExecutorInput},
+    utils::profile_report,
 };
 use std::sync::Arc;
 
 pub fn main() {
     // Read the input.
-    println!("cycle-tracker-report-start: {}", DESERIALZE_INPUTS);
-    let input = sp1_zkvm::io::read_vec();
-    let input = bincode::deserialize::<OpClientExecutorInput>(&input).unwrap();
-    println!("cycle-tracker-report-end: {}", DESERIALZE_INPUTS);
+    let input = profile_report!(DESERIALZE_INPUTS, {
+        let input = sp1_zkvm::io::read_vec();
+        bincode::deserialize::<OpClientExecutorInput>(&input).unwrap()
+    });
 
     // Execute the block.
     let executor = OpClientExecutor::optimism(Arc::new((&input.genesis).try_into().unwrap()));
