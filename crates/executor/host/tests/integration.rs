57c141ca3a0b58212e04d3973a9de38bf8aca380
leruaa <aurelien.catinon@gmail.com>
2025-06-18 06:59:44 -0700

    fix: allow to serialize ChainConfig with bincode

--- crates/executor/host/tests/integration.rs
+++ crates/executor/host/tests/integration.rs
@@ -11,7 +11,7 @@ use rsp_client_executor::{
     BlockValidator, FromInput, IntoInput, IntoPrimitives,
 };
 use rsp_host_executor::{EthHostExecutor, HostExecutor};
-use rsp_primitives::genesis::Genesis;
+use rsp_primitives::genesis::{genesis_from_json, Genesis, OP_SEPOLIA_GENESIS_JSON};
 use rsp_rpc_db::RpcDb;
 use serde::{de::DeserializeOwned, Serialize};
 use tracing_subscriber::{
@@ -40,6 +40,29 @@ async fn test_e2e_optimism() {
         "RPC_10",
         122853660,
         &Genesis::OpMainnet,
+        None,
+    )
+    .await;
+}
+
+#[tokio::test(flavor = "multi_thread")]
+async fn test_e2e_optimism_sepolia() {
+    let alloy_genesis = genesis_from_json(OP_SEPOLIA_GENESIS_JSON).unwrap();
+    let genesis = Genesis::Custom(alloy_genesis.config);
+    let chain_spec: Arc<OpChainSpec> = Arc::new((&genesis).try_into().unwrap());
+
+    // Setup the host executor.
+    let host_executor = rsp_host_executor::OpHostExecutor::optimism(chain_spec.clone());
+
+    // Setup the client executor.
+    let client_executor = rsp_client_executor::executor::OpClientExecutor::optimism(chain_spec);
+
+    run_e2e::<_, OpChainSpec, op_alloy_network::Optimism>(
+        host_executor,
+        client_executor,
+        "RPC_11155420",
+        24000000,
+        &genesis,
         None,
     )
     .await;
